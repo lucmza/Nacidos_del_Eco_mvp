54bc3d50da9d98e363a5e7a11deff9afeabd290f
lucmza <lulucmza@gmail.com>
2025-06-18 20:40:15 -0300

    varios

---
+++ Assets/nTools/PrefabPainter/Scripts/PrefabPainterSceneSettings.cs
@@ -0,0 +1,16 @@
+﻿
+using UnityEngine;
+using System.Collections;
+
+
+namespace nTools.PrefabPainter
+{
+    //
+    // class PrefabPainterSceneSettings
+    //
+    public class PrefabPainterSceneSettings : MonoBehaviour
+    {
+        public GameObject parentForPrefabs;
+    }
+
+}
