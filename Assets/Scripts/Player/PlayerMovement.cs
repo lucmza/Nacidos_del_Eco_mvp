86c0218e2a6a7396d37755f0a648cc002800ac26
Camilo Rodriguez <camiloceatrabajo@gmail.com>
2025-06-19 20:40:27 -0300

    mas arreglos

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -6,9 +6,9 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private float _jumpForce;
     [SerializeField] private float _WalkingSpeed;
     [SerializeField] private float _CrouchingSpeed;
-    private float _speed;
     [SerializeField] private float _gravity = -9.8f;
-    //private Rigidbody _playerRb;
+
+    private float _speed;
     private PlayerInput _playerInput;
     private CapsuleCollider _playerCollider;
     private CharacterController _characterController;
@@ -17,42 +17,29 @@ public class PlayerMovement : MonoBehaviour
     private float _horizontalSpeed;
     private Vector3 _playerVelocity;
 
-    private bool _onStealth = false;
-
     private bool _onGround;
+    private bool _isCrouching;
+    private bool _onStealth;
 
-    //Animation variables
     [SerializeField] private Animator _animator;
-    private bool _isCrouching = false;
-    private bool _IsWalking;
-    private bool _movePressed;
 
-
-    //Audio
+    [Header("Footsteps")]
     [SerializeField] private AudioSource footstepAudioSource;
     [SerializeField] private AudioClip stepGrass;
     [SerializeField] private AudioClip stepConcrete;
-    private bool _wasOnGroundLastFrame = false;
     [SerializeField] private AudioClip landSound;
-    private bool _wasGroundedLastFrame = false;
-    private bool _wasFallingLastFrame = false;
-
-
     [SerializeField] private float stealthStepMultiplier = 1.3f;
     private float baseStepInterval;
     [SerializeField] private float stepInterval = 0.4f;
     private float stepTimer;
+    private float _fallDuration = 0f;
+    [SerializeField] private float _minFallTimeToPlayLandSound = 0.15f;
 
 
-    public bool OnStealth
-    {
-        get { return _onStealth; }
-    }
-
+    public bool OnStealth => _onStealth;
 
     private void Awake()
     {
-        //_playerRb = GetComponent<Rigidbody>();
         _playerInput = GetComponent<PlayerInput>();
         _playerCollider = GetComponent<CapsuleCollider>();
         _characterController = GetComponent<CharacterController>();
@@ -64,49 +51,47 @@ public class PlayerMovement : MonoBehaviour
         baseStepInterval = stepInterval;
     }
 
-
-
     private void Update()
     {
-        bool groundedNow = IsActuallyOnGround();
+        bool wasGroundedLastFrame = _onGround;
+        _onGround = _characterController.isGrounded;
 
-        bool wasFalling = _wasFallingLastFrame;
-
-        _onGround = groundedNow;
-
-        // Detectar aterrizaje solo si estaba cayendo antes
-        if (groundedNow && !_wasGroundedLastFrame && wasFalling)
+        if (!_onGround)
         {
-            PlayLandSound();
+            if (_playerVelocity.y < -0.1f)
+                _fallDuration += Time.deltaTime;
+        }
+        else if (!wasGroundedLastFrame && _onGround)
+        {
+            if (_fallDuration >= _minFallTimeToPlayLandSound)
+            {
+                PlayLandSound();
+            }
+            _fallDuration = 0f;
         }
 
-        // Guardar estados
-        _wasGroundedLastFrame = groundedNow;
-        _wasFallingLastFrame = _playerVelocity.y < -0.1f;
+
 
         _input = _playerInput.actions["Move"].ReadValue<Vector2>();
         _movementRelativeToCamera = MoveRelativeToCamera(_input);
         RotatePlayer();
 
-        if (_onGround && _playerVelocity.y < 0)
-        {
-            _playerVelocity.y = 0;
-        }
-
+        if (_onGround && _playerVelocity.y < 0f)
+            _playerVelocity.y = 0f;
         _playerVelocity.y += _gravity * Time.deltaTime;
 
-        // Animation
+        Vector3 horizontalVel = new Vector3(_movementRelativeToCamera.x, 0, _movementRelativeToCamera.z);
+        _horizontalSpeed = horizontalVel.magnitude;
         _animator.SetFloat("IsWalking", _horizontalSpeed);
 
-        // Pasos
         bool isMoving = _horizontalSpeed > 0.1f && _onGround;
         if (isMoving)
         {
-            stepInterval = _onStealth ? baseStepInterval * stealthStepMultiplier : baseStepInterval;
-
+            float interval = _onStealth
+                ? baseStepInterval * stealthStepMultiplier
+                : baseStepInterval;
             stepTimer += Time.deltaTime;
-            if (stepTimer == 0f) PlayFootstep();
-            if (stepTimer >= stepInterval)
+            if (stepTimer >= interval)
             {
                 PlayFootstep();
                 stepTimer = 0f;
@@ -116,16 +101,20 @@ public class PlayerMovement : MonoBehaviour
         {
             stepTimer = 0f;
         }
-    }
 
+
+    }
 
     private void FixedUpdate()
     {
-        MovePlayer(_movementRelativeToCamera);
-
+        Vector3 movement = new Vector3(
+            _movementRelativeToCamera.x,
+            _playerVelocity.y,
+            _movementRelativeToCamera.z
+        );
+        _characterController.Move(movement * _speed * Time.deltaTime);
     }
 
-    //Deshabilitado por el momento
     public void Jump(InputAction.CallbackContext context)
     {
         if (context.performed && _onGround)
@@ -133,112 +122,75 @@ public class PlayerMovement : MonoBehaviour
             _animator.SetTrigger("Jump");
             _playerVelocity.y = Mathf.Sqrt(_jumpForce * -1f * _gravity);
         }
-
     }
 
-    private void MovePlayer(Vector3 input)
+    public void Crouch(InputAction.CallbackContext ctx)
     {
-        _movePressed = _input.x != 0 || _input.y != 0;
+        if (!ctx.performed) return;
 
-        Vector3 horizontalInput = new Vector3(input.x, 0, input.z);
-        _horizontalSpeed = horizontalInput.magnitude;
+        _isCrouching = !_isCrouching;
+        float standingHeight = 1.8f;
+        float crouchHeight = 0.9f;
 
-        Vector3 movement = new Vector3(input.x, _playerVelocity.y, input.z);
-        _characterController.Move(movement * _speed * Time.deltaTime);
+        if (_isCrouching)
+        {
+            _playerCollider.height = crouchHeight;
+            _playerCollider.center = new Vector3(0, crouchHeight / 2f, 0);
+            _speed = _CrouchingSpeed;
+            _onStealth = true;
+        }
+        else
+        {
+            _playerCollider.height = standingHeight;
+            _playerCollider.center = new Vector3(0, standingHeight / 2f, 0);
+            _speed = _WalkingSpeed;
+            _onStealth = false;
+        }
+
+        _animator.SetBool("IsCrouching", _isCrouching);
     }
-
-
 
     private void RotatePlayer()
     {
-        float rotationSpeed = 5;
-        Vector3 posToLookAt;
-        posToLookAt.x = _movementRelativeToCamera.x;
-        posToLookAt.y = 0;
-        posToLookAt.z = _movementRelativeToCamera.z;
-        posToLookAt = posToLookAt.normalized;
+        Vector3 dir = _movementRelativeToCamera;
+        dir.y = 0;
+        if (dir.sqrMagnitude < 0.01f) return;
 
-
-        Quaternion targetRotation = posToLookAt == Vector3.zero ? transform.rotation : Quaternion.LookRotation(posToLookAt);
-
-        transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+        Quaternion targetRot = Quaternion.LookRotation(dir.normalized);
+        transform.rotation = Quaternion.Slerp(transform.rotation, targetRot, 5f * Time.deltaTime);
     }
 
     private Vector3 MoveRelativeToCamera(Vector2 input)
     {
         Vector3 camF = Camera.main.transform.forward;
         Vector3 camR = Camera.main.transform.right;
-        camF.y = camR.y = 0;
-        camF.Normalize(); camR.Normalize();
-        return input.y * camF + input.x * camR;
-    }
-
-    public void Crouch(InputAction.CallbackContext callbackContext)
-    {
-        float standingHeight = 1.8f;
-        float crouchingHeight = 0.9f;
-
-        if (callbackContext.performed)
-        {
-
-            _isCrouching = !_isCrouching;
-            if (_isCrouching)
-            {
-                _playerCollider.height = crouchingHeight;
-                _playerCollider.center = new Vector3(0, 0.45f, 0);
-                _speed = _CrouchingSpeed;
-                _animator.SetBool("IsCrouching", _isCrouching);
-                _onStealth = true;
-                Debug.Log(_onStealth);
-            }
-            else
-            {
-                _playerCollider.height = standingHeight;
-                _playerCollider.center = new Vector3(0, 0.9f, 0);
-                _speed = _WalkingSpeed;
-                _animator.SetBool("IsCrouching", _isCrouching);
-                _onStealth = false;
-                Debug.Log(_onStealth);
-            }
-        }
-    }
-
-    private bool IsActuallyOnGround()
-    {
-        Vector3 origin = transform.position + Vector3.up * 0.1f;
-        float rayLength = 0.4f;
-
-        return Physics.Raycast(origin, Vector3.down, rayLength);
+        camF.y = 0; camR.y = 0;
+        Vector3 move = input.y * camF + input.x * camR;
+        return move.normalized;
     }
 
     private void PlayFootstep()
     {
+        if (!footstepAudioSource) return;
         Ray ray = new Ray(transform.position + Vector3.up * 0.1f, Vector3.down);
         if (Physics.Raycast(ray, out RaycastHit hit, 1.5f))
         {
-            string tag = hit.collider.tag;
-            AudioClip clipToPlay = stepGrass;
+            AudioClip clip = hit.collider.CompareTag("Concrete")
+                ? stepConcrete
+                : stepGrass;
 
-            if (tag == "Concrete") clipToPlay = stepConcrete;
-            else if (tag == "Grass") clipToPlay = stepGrass;
-
-            if (footstepAudioSource && clipToPlay)
+            if (clip != null)
             {
                 footstepAudioSource.pitch = Random.Range(0.9f, 1.1f);
-
-                footstepAudioSource.PlayOneShot(clipToPlay);
+                footstepAudioSource.PlayOneShot(clip);
             }
-
         }
     }
 
     private void PlayLandSound()
     {
-        if (footstepAudioSource && landSound)
-        {
-            footstepAudioSource.pitch = Random.Range(0.95f, 1.05f);
-            footstepAudioSource.PlayOneShot(landSound);
-        }
+        if (!footstepAudioSource || landSound == null) return;
+        footstepAudioSource.pitch = Random.Range(0.95f, 1.05f);
+        footstepAudioSource.PlayOneShot(landSound);
     }
-
 }
